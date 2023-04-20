48a6098c49e4317177c7bcfab459fc13543ebe9a
Sharon Raj Parwar <103627046+SharonRaj05@users.noreply.github.com>
2023-04-20 06:46:34 +0530

    Add files via upload

---
+++ minorproject.py
@@ -0,0 +1,149 @@
+#localization of number plate or highlighting of licenseplate
+from skimage.io import imread
+from skimage.filters import threshold_otsu
+import matplotlib.pyplot as plt
+from skimage import measure
+from skimage.measure import regionprops
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import localization
+
+
+
+car_image = imread("pics and videos\car.jpg", as_gray=True)
+
+print(car_image.shape)
+
+
+gray_car_image = car_image * 255
+fig, (ax1, ax2) = plt.subplots(1, 2)
+ax1.imshow(gray_car_image, cmap="gray")
+threshold_value = threshold_otsu(gray_car_image)
+binary_car_image = gray_car_image > threshold_value
+ax2.imshow(binary_car_image, cmap="gray")
+plt.show()
+##########################################################
+
+#using connected component analysis we find the pixels
+label_image = measure.label(binary_car_image)
+fig, (ax1) = plt.subplots(1)
+ax1.imshow(gray_car_image, cmap="gray");
+
+for region in regionprops(label_image):
+    if region.area < 50:
+       
+        continue
+
+    
+    minRow, minCol, maxRow, maxCol = region.bbox
+    rectBorder = patches.Rectangle((minCol, minRow), maxCol-minCol, maxRow-minRow, edgecolor="red", linewidth=2, fill=False)
+    ax1.add_patch(rectBorder)
+
+plt.show()
+############################################################
+
+
+# this gets all the connected regions and groups them together
+label_image = measure.label(binary_car_image)
+
+# getting the maximum width, height and minimum width and height that a license plate can be
+plate_dimensions = (0.08*label_image.shape[0], 0.2*label_image.shape[0], 0.15*label_image.shape[1], 0.4*label_image.shape[1])
+min_height, max_height, min_width, max_width = plate_dimensions
+plate_objects_cordinates = []
+plate_like_objects = []
+fig, (ax1) = plt.subplots(1)
+ax1.imshow(gray_car_image, cmap="gray");
+
+# regionprops creates a list of properties of all the labelled regions
+for region in regionprops(label_image):
+    if region.area < 50:
+        #if the region is so small then it's likely not a license plate
+        continue
+
+    # the bounding box coordinates
+    min_row, min_col, max_row, max_col = region.bbox
+    region_height = max_row - min_row
+    region_width = max_col - min_col
+    # ensuring that the region identified satisfies the condition of a typical license plate
+    if region_height >= min_height and region_height <= max_height and region_width >= min_width and region_width <= max_width and region_width > region_height:
+        plate_like_objects.append(localization.binary_car_image[min_row:max_row,
+                                  min_col:max_col])
+        plate_objects_cordinates.append((min_row, min_col,
+                                              max_row, max_col))
+        rectBorder = patches.Rectangle((min_col, min_row), max_col-min_col, max_row-min_row, edgecolor="red", linewidth=2, fill=False)
+        ax1.add_patch(rectBorder)
+    # let's draw a red rectangle over those regions
+
+plt.show()
+############################################
+import os
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+import joblib
+from skimage.io import imread
+from skimage.filters import threshold_otsu
+
+letters = [
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z'
+        ]
+
+def read_training_data(training_directory):
+    image_data = []
+    target_data = []
+    for each_letter in letters:
+        for each in range(10):
+            image_path = os.path.join(training_directory, each_letter, each_letter + '_' + str(each) + '.jpg')
+            # read each image of each character
+            img_details = imread(image_path, as_gray=True)
+            # converts each character image to binary image
+            binary_image = img_details < threshold_otsu(img_details)
+            # the 2D array of each image is flattened because the machine learning
+            # classifier requires that each sample is a 1D array
+            # therefore the 20*20 image becomes 1*400
+            # in machine learning terms that's 400 features with each pixel
+            # representing a feature
+            flat_bin_image = binary_image.reshape(-1)
+            image_data.append(flat_bin_image)
+            target_data.append(each_letter)
+
+    return (np.array(image_data), np.array(target_data))
+
+def cross_validation(model, num_of_fold, train_data, train_label):
+    # this uses the concept of cross validation to measure the accuracy
+    # of a model, the num_of_fold determines the type of validation
+    # e.g if num_of_fold is 4, then we are performing a 4-fold cross validation
+    # it will divide the dataset into 4 and use 1/4 of it for testing
+    # and the remaining 3/4 for the training
+    accuracy_result = cross_val_score(model, train_data, train_label,
+                                      cv=num_of_fold)
+    print("Cross Validation Result for ", str(num_of_fold), " -fold")
+
+    print(accuracy_result * 100)
+
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+training_dataset_dir = os.path.join(current_dir, 'train')
+
+image_data, target_data = read_training_data(training_dataset_dir)
+
+# the kernel can be 'linear', 'poly' or 'rbf'
+# the probability was set to True so as to show
+# how sure the model is of it's prediction
+svc_model = SVC(kernel='linear', probability=True)
+
+cross_validation(svc_model, 4, image_data, target_data)
+
+# let's train the model with all the input data
+svc_model.fit(image_data, target_data)
+
+# we will use the joblib module to persist the model
+# into files. This means that the next time we need to
+# predict, we don't need to train the model again
+save_directory = os.path.join(current_dir, 'models/svc/')
+if not os.path.exists(save_directory):
+    os.makedirs(save_directory)
+joblib.dump(svc_model, save_directory+'/svc.pkl')
